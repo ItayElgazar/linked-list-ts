6ccdb79ba1b30f892f6ec41827d973b93705e8ab
Itay Elgazar <Itayekk1@gmail.com>
2018-10-06 13:24:12 +0200

    folders stcutrue changed

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var item_1 = require("./item");
+var item_1 = require("../item");
 var LinkedList = (function () {
     function LinkedList() {
     }
